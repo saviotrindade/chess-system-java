6bded84b9529fb656cbfd7473739d8c188d7a81c
Sávio Bandeira <savio-bandeira@hotmail.com>
2024-06-11 23:17:27 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,15 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
+
 
 public class UI {
+
+    public static final String ANSI_WHITE = "\u001b[0m";
+    public static final String ANSI_YELLOW = "\u001b[33m";
+    public static final String ANSI_RESET = "\u001b[37m";
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i<pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -19,7 +26,12 @@ public class UI {
             System.out.print("-");
         }
         else {
-            System.out.print(piece);
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         System.out.print(" ");
     }
